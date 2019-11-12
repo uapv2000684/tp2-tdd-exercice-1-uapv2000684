4638daf22a9452a36aee27667b3a38508ced2b18
zouhairmaazouz <zouhirmaazouz@gmail.com>
2019-11-12 14:47:44 +0100

    Deuxieme Refactor

--- src/test/java/exo1/FizzBuzzTest.java
+++ src/test/java/exo1/FizzBuzzTest.java
@@ -6,18 +6,15 @@ import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
-
+	FizzBuzz fizzbuzz=new FizzBuzz();
+	
 	@Test
 	void doitRetourner1AvecEntree1() {
-//		fail("Not yet implemented");
-		FizzBuzz fizzbuzz=new FizzBuzz();
 		assertTrue(fizzbuzz.getResult(1).equals("1"));
 	}
 
 	@Test
 	void doitRetourner1AvecEntree2() {
-//		fail("Not yet implemented");
-		FizzBuzz fizzbuzz=new FizzBuzz();
 		assertTrue(fizzbuzz.getResult(2).equals("2"));
 	}
 	
